69abdc94d64bdb487b5403229b9b3c8e1ff537a2
Seojeonguk <uk7880@naver.com>
2021-03-10 18:03:55 +0900

    update version2

--- WebContent/JS/content.js
+++ WebContent/JS/content.js
@@ -1,119 +1,129 @@
-window.onload = function() {
-	
-	downloadtext();
+$(function() {
+	var currentPosition = parseInt($("#floatMenu").css("top"));
+	$(window).scroll(function() {
+		var position = $(window).scrollTop();
+		$("#floatMenu").stop().animate({"top":position+currentPosition+"px"},1000);
+	});
+	$("#header").load("header.html"); 
+	$("#footer").load("footer.html"); 
 
+	$.ajax({
+		url: "img/볼거리먹거리json.json",
+		type: "GET",
+		dataType : 'json',
+		success:function(response) {
+			create_div2(response);
+		},
+		error:function(xhr,status,msg) {
+			console.log("상태값 : " + status + " Http에러메시지 : " + msg);
+		}
+	});
 
-	list_item = document.getElementsByClassName("list-item");
-
-	document.getElementById("sea-btn").onclick = function() {
+	$("#sea-btn").click(function() {
 		not_hide_list_item("sea-item");
-	};
+	});
 
-
-	document.getElementById("eat-btn").onclick = function() {
+	$("#eat-btn").click(function() {
 		not_hide_list_item("eat-item");
-	};
+	});
 
-	document.getElementById("gwangsan").onclick = function() {
-		not_hide_list_item("gwangsan-item");
-	};
+	$(".gu-btn").each(function() {
+		$(this).click(function() {
+			not_hide_list_item($(this).attr("id")+"-item");
+		});
+	});
 
-	document.getElementById("seogu").onclick = function() {
-		not_hide_list_item("seogu-item");
-	};
+	// $(".btn_list").each(function() {
+	// 	$(this).click(function() {
+	// 		not_hide_list_item($(this).attr("id").slice(0,4)+"item");
+	// 	});
+	// });
 
-	document.getElementById("namgu").onclick = function() {
-		not_hide_list_item("namgu-item");
-	};
-
-	document.getElementById("donggu").onclick = function() {
-		not_hide_list_item("donggu-item");
-	};
-
-	document.getElementById("bukgu").onclick = function() {
-		not_hide_list_item("bukgu-item");
-	};
-
-	document.getElementById("up").onclick = function() {
+	$("#up").click(function() {
 		window.scrollTo(0,0);
-	};
+	});
 
-	document.getElementById("down").onclick = function() {
-		var logo = document.getElementById("logo");
-		window.scrollTo(0,document.body.offsetHeight);
-	};
-};
+	$("#down").click(function() {
+		window.scrollTo(0,document.body.offsetHeight);		
+	});
+});
 
 function not_hide_list_item(str) {
-	console.log(str);
-	for(var i=0;i<list_item.length;i++) {
-		var class_list = list_item[i].className.split(' ');
+	$(".hide").each(function() {
+		$(this).removeClass('hide');
+	});
 
-		// 해당하는 클래스가 아닐경우
-		if(class_list.indexOf(str)==-1) {
-			// hide클래스가없으면 추가
-			if(class_list.indexOf('hide')==-1) {
-				list_item[i].className += " " + 'hide';
-			}
-		}
-		else {
-			//hide클래스가 있으면 제거
-			if(class_list.indexOf('hide')!=-1) {
-				var check = new RegExp("(\\s|^)" + 'hide' + "(\\s|$)"); 
-				list_item[i].className = list_item[i].className.replace(check, " ").trim();
-
-			}
-		}
-	}
+	$(".list-item").not("."+str).each(function() {
+		$(this).addClass('hide');
+	});
 }
 
-async function downloadtext() {
-	const res = await fetch('./img/볼거리먹거리.txt');
-	const text = await res.text();
-	arr = text.split('\n');
+function create_div2(data) {
+	$.each(data,function(index,item) {
+		var $new_div = $(`<div class="list-item"></div>`).appendTo("#list");
+		if(item.gu=="광산구") $new_div.addClass("gwangsan-item");
+		else if(item.gu=="동구") $new_div.addClass("donggu-item");
+		else if(item.gu=="서구") $new_div.addClass("seogu-item");
+		else if(item.gu=="남구") $new_div.addClass("namgu-item");
+		else if(item.gu=="북구") $new_div.addClass("bukgu-item");
+
+		if(item.category=='먹거리') $new_div.addClass("eat-item");
+		else if(item.category == '볼거리') $new_div.addClass("sea-item");
+
+		var $list_img = $('<div>',{
+			class: 'list-img'
+		}).appendTo($new_div);
+
+		var str = item.gu + "_";
+		if(item.category=='먹거리') str += "먹_";
+		str+=item.title.replace(' ','') + ".jpg";
+		$('<img>',{
+			src : './img/' + str
+		}).appendTo($list_img);
+
+
+		var $list_info = $('<div>', {
+			class : 'list-info'
+		}).appendTo($new_div);
+		
+		var $title = $('<p class="item-title">').text(item.title);
+
+		//var $category = $('<span class="badge">').html(item.category[0]);
+		var $category = $('<span class="badge">').html(String.fromCodePoint(0x1F60E));
+		if(item.category=='먹거리') {
+			var $category = $('<span class="badge">').html(String.fromCodePoint(0x1F370));
+			$category.addClass('badge-info');
+		}
+		else if(item.category=='볼거리') {
+			var $category = $('<span class="badge">').html(String.fromCodePoint(0x1F60E));
+			$category.addClass('badge-warning');
+		}
+		$title.append($category).appendTo($list_info);
 	
-	create_div();
-}
 
-function create_div() {
-	var list = document.getElementById("list");
-	for(var i=0;i<arr.length;i++) {
-		brr = arr[i].split('|');
+		$('<p class="item-gu">').html(item.gu).appendTo($list_info);
 
-		var item_div = document.createElement('div');
-		item_div.setAttribute('class','list-item');
-		if(brr[0]=="광산구") item_div.className += " " + "gwangsan-item";
-		else if(brr[0]=="동구") item_div.className += " " + "donggu-item";
-		else if(brr[0]=="서구") item_div.className += " " + "seogu-item";
-		else if(brr[0]=="남구") item_div.className += " " + "namgu-item";
-		else if(brr[0]=="북구") item_div.className += " " + "bukgu-item";
-
-		if(brr[1]=='먹거리') item_div.className += " " + "eat-item";
-		else if(brr[1] == '볼거리') item_div.className += " " + "sea-item";
-		var img_div = document.createElement('div');
-		img_div.setAttribute('class','list-img');
-
-		var str = brr[0] + "_";
-		if(brr[1]=='먹거리') str += "먹_";
-		str+=brr[2].replace(' ','') + ".jpg";
-
-		var img = document.createElement('img');
-		img.setAttribute('src','./img/' +str);
-		img_div.appendChild(img);
-
-		var item_info_div = document.createElement('div');
-		item_info_div.setAttribute('class','list-info');
-
-
-		for(var j=0;j<brr.length;j++) {
-			var p = document.createElement('p');
-			p.innerHTML = brr[j];
-			item_info_div.appendChild(p);
+		if("" != item.star) {
+			var $star = $('<p class="grade row">');
+			$('<div class="star">').text(String.fromCodePoint(0x1F496)).appendTo($star);
+			$star.html($star.html() + item.star.slice(1));
+			$star.appendTo($list_info);
 		}
+		$('<p class="item-place">').html(item.place).appendTo($list_info);
+		$('<p class="item-waytogo">').html(item.waytogo).appendTo($list_info);
 
-		item_div.appendChild(img_div);
-		item_div.appendChild(item_info_div);
-		list.appendChild(item_div);
 
-	}
+		var hash_list = item.hashtag.split(',')
+		var $hash_tag_p = $('<p class="hash_list">');
+		var instagram_search_url = 'https://www.instagram.com/explore/tags/';
+		if("" != hash_list) {
+			$.each(hash_list,function(index,item) {
+				$hash_item = $('<a class="hashtag">').text("#" + item);
+				$hash_item.attr('href', instagram_search_url + item.replaceAll(' ',''));
+				$hash_item.attr('target','_blank');
+				$hash_item.appendTo($hash_tag_p);
+			});
+		}
+		$hash_tag_p.appendTo($list_info);
+	});
 }
---
+++ WebContent/JS/header.js
@@ -0,0 +1,7 @@
+$(function() {
+	$(".menu-item").each(function() {
+		$(this).click(function() {
+			alert('아직 준비되지 않은 메뉴입니다.');
+		});
+	});
+});
